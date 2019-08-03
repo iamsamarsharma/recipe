81b0d9d24a47da89c5f8110fd0b4d515405c67bc
samar@DESKTOP-GAVQ5IT <samarsharma@outlook.com>
2019-08-04 04:28:46 +0530

    initial project structor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {RecipeModel} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipe: RecipeModel[] = [
+    new RecipeModel('Burger', 'paneerBurger', 'https://www.google.com/search?rlz=1C1CHBD_enIN858IN859&biw=1522&bih=704&tbm=isch&sa=1&ei=CQ9GXa2fA9iCyAPY6KzABQ&q=vada+pav&oq=vada+pao&gs_l=img.3.0.0i10l10.15764.22065..24504...0.0..0.375.3011.0j12j1j3......0....1..gws-wiz-img.......0i7i30.GTC82A_3oAs#imgrc=R72S4uep3yzs2M:')
+  ];
   constructor() { }
 
   ngOnInit() {
